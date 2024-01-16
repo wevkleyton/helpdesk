ef0a6713d06bfb6038408fa5be9ffeef23f452e5
wevkleyton <kleyton.cpd@gmail.com>
2024-01-16 17:44:13 -0300

    Anotacoes JPA para classes de dominio

--- src/main/java/com/suporte/helpdesk/domain/Chamado.java
+++ src/main/java/com/suporte/helpdesk/domain/Chamado.java
@@ -1,21 +1,45 @@
 package com.suporte.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.suporte.helpdesk.domain.enums.Prioridade;
 import com.suporte.helpdesk.domain.enums.Status;
 
-public class Chamado {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
+public class Chamado implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/MM/yyy")
 	private LocalDate dataFechamento;
+	
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 
 	public Chamado() {
--- src/main/java/com/suporte/helpdesk/domain/Tecnico.java
+++ src/main/java/com/suporte/helpdesk/domain/Tecnico.java
@@ -3,16 +3,27 @@ package com.suporte.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.suporte.helpdesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa{
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
