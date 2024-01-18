95e2127126b5fb3ab7d12d10e9c13764b1ee3a2c
wevkleyton <kleyton.cpd@gmail.com>
2024-01-18 17:27:00 -0300

    Remocao do importa da anotacao do Bean

--- src/main/java/com/suporte/helpdesk/config/TestConfig.java
+++ src/main/java/com/suporte/helpdesk/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.suporte.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
