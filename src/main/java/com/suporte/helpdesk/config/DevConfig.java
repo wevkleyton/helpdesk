348fe7b4266d0c15821313a520d257c9bb90a088
wevkleyton <kleyton.cpd@gmail.com>
2024-01-19 16:54:16 -0300

    perfil de desenvolvimento

---
+++ src/main/java/com/suporte/helpdesk/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.suporte.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.suporte.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	public boolean instanciaDB() {
+		if (value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
+	}
+}
