d706f4c9ca6959553aebd8f46f39e113aac7590c
wevkleyton <kleyton.cpd@gmail.com>
2024-01-25 17:16:23 -0300

    endpoint findAll para Tecnico

--- src/main/java/com/suporte/helpdesk/recouces/TecnicoResouce.java
+++ src/main/java/com/suporte/helpdesk/recouces/TecnicoResouce.java
@@ -1,5 +1,9 @@
 package com.suporte.helpdesk.recouces;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +26,14 @@ public class TecnicoResouce {
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+	}
+	
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
