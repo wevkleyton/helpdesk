ef50a603b6e9e83d54ce0aa0111efe5a87f61497
wevkleyton <kleyton.cpd@gmail.com>
2024-02-22 17:04:26 -0300

    Update para chamado implementado

--- src/main/java/com/suporte/helpdesk/services/ChamadoService.java
+++ src/main/java/com/suporte/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.suporte.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,6 +41,13 @@ public class ChamadoService {
 
 		return repository.save(newChamado(objDTO));
 	}
+	
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
 
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -49,6 +57,11 @@ public class ChamadoService {
 		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
+		
+		if (obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
+		
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -58,4 +71,6 @@ public class ChamadoService {
 
 		return chamado;
 	}
+
+
 }
